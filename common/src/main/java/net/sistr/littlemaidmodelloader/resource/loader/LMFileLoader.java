31f9d15b6f368eeed96f697174f08a8f0b89f34b
SistrScarlet <game.sistr@gmail.com>
2022-11-30 12:29:15 +0900

    リファクタ

--- common/src/main/java/net/sistr/littlemaidmodelloader/resource/loader/LMFileLoader.java
+++ common/src/main/java/net/sistr/littlemaidmodelloader/resource/loader/LMFileLoader.java
@@ -1,15 +1,20 @@
 package net.sistr.littlemaidmodelloader.resource.loader;
 
+import net.minecraft.util.Util;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.MalformedInputException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
 import java.util.zip.ZipInputStream;
 
 public class LMFileLoader {
@@ -46,10 +51,10 @@ public class LMFileLoader {
                 if (Files.notExists(folderPath)) {
                     Files.createDirectory(folderPath);
                 }
-                try (Stream<Path> stream = Files.walk(folderPath)) {
-                    stream.filter(path -> !Files.isDirectory(path))
-                            .forEach(path -> fileLoad(folderPath, path));
-                }
+                Stream<Path> stream = Files.walk(folderPath);
+                stream.filter(path -> !Files.isDirectory(path))
+                        .forEach(path -> fileLoad(folderPath, path));
+                stream.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -67,38 +72,60 @@ public class LMFileLoader {
     }
 
     public void loadArchive(Path folderPath, Path path) {
-        ZipInputStream zipStream;
-        try {
-            zipStream = new ZipInputStream(Files.newInputStream(path));
-        } catch (Exception e) {
-            LOGGER.debug("Can't load Zip! : " + path);
-            e.printStackTrace();
-            return;
-        }
-        try {
-            ZipEntry entry;
-            while ((entry = zipStream.getNextEntry()) != null) {
-                ZipEntry finalEntry = entry;
-                loaders.stream().filter(loader -> loader.canLoad(finalEntry.getName(), path, zipStream, true))
-                        .forEach(loader -> loader.load(finalEntry.getName(), path, zipStream, true));
+        if (!loadArchive(folderPath, path, StandardCharsets.UTF_8)
+                && Util.getOperatingSystem() == Util.OperatingSystem.WINDOWS) {
+            LOGGER.info("MS932でリトライします。 : " + path);
+            if (loadArchive(folderPath, path, Charset.forName("MS932"))) {
+                LOGGER.info("読み込みに成功。");
+            } else {
+                LOGGER.error("読み込みに失敗。");
             }
-        } catch (Exception e) {
-            LOGGER.debug("Zipの読み込み中にエラーが発生。ファイルネームに日本語などが入ってると読み込めません。" +
-                    "An error occurs while reading a Zip file. If the file name contains Japanese characters, " +
-                    "it cannot be read. : " + path);
-            e.printStackTrace();
         }
     }
 
-    public void loadFile(Path folderPath, Path path) {
-        try {
-            String relPath = path.toString().replace(folderPath.toString(), "");
-            try (InputStream inputStream = Files.newInputStream(path)) {
-                loaders.stream().filter(loader -> loader.canLoad(relPath, folderPath, inputStream, false))
-                        .forEach(loader -> loader.load(relPath, folderPath, inputStream, false));
+    private boolean loadArchive(Path folderPath, Path path, Charset charset) {
+        boolean result = true;
+        try (ZipInputStream zipStream = new ZipInputStream(Files.newInputStream(path), charset)) {
+            while (true) {
+                ZipEntry entry = zipStream.getNextEntry();
+                if (entry == null) {
+                    break;
+                }
+                loaders.stream().filter(loader -> loader.canLoad(entry.getName(), path, zipStream, true))
+                        .forEach(loader -> loader.load(entry.getName(), path, zipStream, true));
             }
+        } catch (ZipException e) {
+            LOGGER.error("Zipの読み込み中にエラーが発生。" +
+                    "ファイル名に日本語などが入っている可能性があります。" +
+                    "Error while loading Zip, " +
+                    "possibly due to Japanese characters in the file name. : " + path);
+            result = false;
+        } catch (IllegalArgumentException e) {
+            if (e.getCause() instanceof MalformedInputException) {
+                LOGGER.error("Zipの読み込み中にエラーが発生。" +
+                        "Zip内のファイル名に日本語などが入っている可能性があります。" +
+                        "Error while loading Zip, " +
+                        "possibly due to Japanese characters in the file name in the Zip. : " + path);
+            } else {
+                LOGGER.error("不明なエラーによりZipが読み込めません。" +
+                        "Unknown error prevents Zip from loading. : " + path);
+            }
+            result = false;
         } catch (Exception e) {
-            LOGGER.debug("Error! : " + e.getMessage() + " : " + path);
+            LOGGER.error("不明なエラーによりZipが読み込めません。" +
+                    "Unknown error prevents Zip from loading. : " + path);
+            result = false;
+        }
+        return result;
+    }
+
+    public void loadFile(Path folderPath, Path path) {
+        String relPath = path.toString().replace(folderPath.toString(), "");
+        try (InputStream inputStream = Files.newInputStream(path)) {
+            loaders.stream().filter(loader -> loader.canLoad(relPath, folderPath, inputStream, false))
+                    .forEach(loader -> loader.load(relPath, folderPath, inputStream, false));
+        } catch (Exception e) {
+            LOGGER.error("Error! : " + e.getMessage() + " : " + path);
         }
     }
 
