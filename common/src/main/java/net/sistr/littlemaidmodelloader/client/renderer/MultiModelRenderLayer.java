2710fce511096aa754a22bfeefe200b7f77975d8
sistr <game.sistr@gmail.com>
2023-05-04 08:19:00 +0900

    半透明のコンフィグを追加

---
+++ common/src/main/java/net/sistr/littlemaidmodelloader/client/renderer/MultiModelRenderLayer.java
@@ -0,0 +1,16 @@
+package net.sistr.littlemaidmodelloader.client.renderer;
+
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.util.Identifier;
+import net.sistr.littlemaidmodelloader.LMMLMod;
+
+public class MultiModelRenderLayer {
+
+    public static RenderLayer getDefault(Identifier identifier) {
+        if (LMMLMod.getConfig().isEnableAlpha()) {
+            return RenderLayer.getEntityTranslucent(identifier);
+        }
+        return RenderLayer.getEntityCutout(identifier);
+    }
+
+}
