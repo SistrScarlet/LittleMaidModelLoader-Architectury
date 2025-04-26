1463d488e35fba68460c2f462d08c34054a101c2
sistr <game.sistr@gmail.com>
2025-04-26 12:27:34 +0900

    テストモデルエンティティのスニーク切り替え追加

--- common/src/main/java/net/sistr/littlemaidmodelloader/entity/MultiModelEntity.java
+++ common/src/main/java/net/sistr/littlemaidmodelloader/entity/MultiModelEntity.java
@@ -100,6 +100,12 @@ public class MultiModelEntity extends PathAwareEntity implements IHasMultiModel,
 
     @Override
     protected ActionResult interactMob(PlayerEntity player, Hand hand) {
+        if (player.isSneaking()) {
+            if (!getWorld().isClient()) {
+                this.setSneaking(!this.isSneaking());
+            }
+            return ActionResult.success(player.getWorld().isClient);
+        }
         ItemStack stack = player.getStackInHand(hand);
         if (stack.getItem() instanceof ArmorItem) {
             ArmorItem armor = (ArmorItem) stack.getItem();
